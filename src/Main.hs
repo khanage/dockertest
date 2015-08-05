3869ce41d264700540f0cb0462ffe84d888767b1
Khan Thompson <khanage@gmail.com>
2015-08-05 15:15:24 +1000

    Adding Main source file

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import qualified Data.Text.Lazy     as T
+import           System.Environment
+import           Web.Scotty
+
 main :: IO ()
-main = putStrLn "dockertest!"
+main = do
+  env <- lookupEnv "THING"
+  scotty 3000 $ do
+    get "/" (text (T.pack (maybe "couldn't load THING" id env)))
+
